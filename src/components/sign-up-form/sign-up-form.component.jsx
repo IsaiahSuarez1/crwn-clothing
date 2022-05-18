3a8fef5b729247f950fbe06a69b192df95cfdf41
IsaiahSuarez1 <102257749+IsaiahSuarez1@users.noreply.github.com>
2022-05-18 12:20:03 -0400

    ButtonStlyesAdded

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import  FormInput  from '../../components/form-input/form-input.component';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import './sign-up-form.styles.scss';
+import Button from '../button/button.component';
 
 const defaultFormFields = {
     displayName: '',
@@ -46,14 +48,15 @@ const SignUpForm = () => {
     }
 
     return(
-        <div>
-            <h1>Enter your email and PW</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <span>Signup with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label = "Display Name" type= "text" required onChange={handleChange} name="displayName" value={displayName}/>
                 <FormInput label = "Email Address" type = "email" required onChange={handleChange} name="email" value={email}/>
                 <FormInput label = "Password" type = "password" required onChange={handleChange} name="password" value={password}/>
                 <FormInput label = "Confirm Password" type = "password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
