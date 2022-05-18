782b9e69e56b40929eb32925be548cfc67439710
IsaiahSuarez1 <102257749+IsaiahSuarez1@users.noreply.github.com>
2022-05-18 11:04:00 -0400

    5.18.Prior-LS100

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import  FormInput  from '../../components/form-input/form-input.component';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
@@ -48,14 +49,10 @@ const SignUpForm = () => {
         <div>
             <h1>Enter your email and PW</h1>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input type= "text" required onChange={handleChange} name="displayName" value={displayName}/>
-                <label>Email Address</label>
-                <input type = "email" required onChange={handleChange} name="email" value={email}/>
-                <label>Password</label>
-                <input type = "password" required onChange={handleChange} name="password" value={password}/>
-                <label>Confirm Password</label>
-                <input type = "password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+                <FormInput label = "Display Name" type= "text" required onChange={handleChange} name="displayName" value={displayName}/>
+                <FormInput label = "Email Address" type = "email" required onChange={handleChange} name="email" value={email}/>
+                <FormInput label = "Password" type = "password" required onChange={handleChange} name="password" value={password}/>
+                <FormInput label = "Confirm Password" type = "password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
                 <button type="submit">Sign Up</button>
             </form>
         </div>
