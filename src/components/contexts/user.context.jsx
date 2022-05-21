29e051e01acb41c8dc128129430fcb0f4e6ce964
IsaiahSuarez1 <102257749+IsaiahSuarez1@users.noreply.github.com>
2022-05-21 14:47:39 -0400

    update

--- src/components/contexts/user.context.jsx
+++ src/components/contexts/user.context.jsx
@@ -1,10 +1,13 @@
 import { createContext, useState } from "react";
 
 export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null,
 
 });
 
 export const UserProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null);
-  return <UserContext.Provider value= {}>{children}</UserContext.Provider>
+    const value = {currentUser, setCurrentUser};
+  return <UserContext.Provider value= {value}>{children}</UserContext.Provider>
 }
