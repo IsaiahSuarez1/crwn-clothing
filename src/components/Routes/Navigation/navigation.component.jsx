25ef41f5a53213a4b13ecf33b1ff108289437be2
IsaiahSuarez1 <102257749+IsaiahSuarez1@users.noreply.github.com>
2022-06-21 14:19:01 -0400

    6.21.22

--- src/components/Routes/Navigation/navigation.component.jsx
+++ src/components/Routes/Navigation/navigation.component.jsx
@@ -2,11 +2,15 @@ import { Fragment, useContext } from 'react';
 import {Outlet, Link} from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
 import { signOutUser } from '../../../utils/firebase/firebase.utils';
+import CartIcon from '../../cart-icon/cart-icon.component';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import './navigation.styles.scss';
+import CartDropdown from '../../cart-dropdown/cart-dropdown.component';
 
 const NavigationBar = () => {
     const {currentUser} = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
     
 
     return(
@@ -27,6 +31,10 @@ const NavigationBar = () => {
                       SIGN-IN
                       </Link>
                     )}
+                    <div>
+                      <CartIcon />
+                    </div>
+                    {isCartOpen && <CartDropdown />}
             </div>
         </div>
         <Outlet />
